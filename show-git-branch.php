1114fddbc2cd05a46a82ef41535cf9d27f780165
swaincreates <ssstrickland@gmail.com>
2015-08-21 14:37:47 -0400

    change order of checking for git locations

--- show-git-branch.php
+++ show-git-branch.php
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 function show_git_branch( $wp_admin_bar ){
         
         //get_home_path requires following file.. may mess up if wp is in another directory?
-        require_once(ABSPATH . 'wp-admin/includes/file.php');
+        require_once( ABSPATH . 'wp-admin/includes/file.php' );
 
     	//get root path from WP dir
     	$root_path = get_home_path() . '.git/HEAD';
@@ -29,12 +29,12 @@ function show_git_branch( $wp_admin_bar ){
     	
     	
         //get branch name
-    	if ( file_exists($root_path)) {
-    		$stringfromfile = file($root_path);
-    		$branchname = implode('/', array_slice(explode('/', file_get_contents($root_path)), 2));
-    	} elseif ( file_exists($theme_path) ) {
-    		$stringfromfile = file($theme_path);
-    		$branchname = implode('/', array_slice(explode('/', file_get_contents($theme_path)), 2));
+    	if ( file_exists( $theme_path )) {
+    		$stringfromfile = file( $theme_path );
+    		$branchname = implode('/', array_slice( explode('/', file_get_contents($theme_path) ), 2) );
+        } elseif ( file_exists( $root_path ) ) {
+            $stringfromfile = file( $root_path );
+            $branchname = implode('/', array_slice( explode('/', file_get_contents( $root_path) ), 2) );
     	} else {
     		$branchname	= "No git detected.";
     	}
@@ -46,11 +46,11 @@ function show_git_branch( $wp_admin_bar ){
 						);
 	$wp_admin_bar->add_node( $args );
 }
-add_action( 'admin_bar_menu', 'show_git_branch', 999);
+add_action( 'admin_bar_menu', 'show_git_branch', 999 );
 
 function dont_add_css_http_request(){
     echo '<style type="text/css">#wp-admin-bar-show-git-branch .ab-item:before {content: "\f237";top:2px;}</style>';
 }
-add_action( 'admin_head', 'dont_add_css_http_request');
-add_action('wp_head', 'dont_add_css_http_request');
+add_action( 'admin_head', 'dont_add_css_http_request' );
+add_action( 'wp_head', 'dont_add_css_http_request' );
 
